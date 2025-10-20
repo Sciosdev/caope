647af3a94a55c3cb75e0bcdaf3ae79643b0fa121
Sciosdev <167479188+Sciosdev@users.noreply.github.com>
2025-10-19 23:05:12 -0600

    Adjust user profile fields migrations

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('carrera', 60)->nullable();
-            $table->string('turno', 40)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
